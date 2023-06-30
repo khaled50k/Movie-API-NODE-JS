c5263cd7d213b2675491965c42892b57f1fcb21b
Khaled Walead Abu Eriban <91320064+khaled50k@users.noreply.github.com>
2023-06-30 11:46:06 +0300

    updating

--- index.js
+++ index.js
@@ -26,46 +26,47 @@ app.use(
 const connectDB = require("./db"); // Import the connectDB function from db.js
 connectDB(); // Call the connectDB function to establish the database connection
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
   // Create a new series
   const newSeries = new Series({
-    title: 'Stranger Things',
-    story: 'A group of young friends in a small town uncover dark secrets when a mysterious girl with telekinetic powers appears.',
-    categories: ['Science Fiction', 'Horror'],
+    title: "Stranger Things",
+    story:
+      "A group of young friends in a small town uncover dark secrets when a mysterious girl with telekinetic powers appears.",
+    categories: ["Science Fiction", "Horror"],
     seasons: [
       {
-        title: 'Season 1',
+        title: "Season 1",
         number: 1,
-        poster: 'https://example.com/season1-poster.jpg',
+        poster: "https://example.com/season1-poster.jpg",
         episodes: [
           {
-            title: 'Chapter One: The Vanishing of Will Byers',
-            url: 'https://example.com/episode1'
+            title: "Chapter One: The Vanishing of Will Byers",
+            url: "https://example.com/episode1",
           },
           {
-            title: 'Chapter Two: The Weirdo on Maple Street',
-            url: 'https://example.com/episode2'
-          }
-        ]
+            title: "Chapter Two: The Weirdo on Maple Street",
+            url: "https://example.com/episode2",
+          },
+        ],
       },
       {
-        title: 'Season 2',
+        title: "Season 2",
         number: 2,
-        poster: 'https://example.com/season2-poster.jpg',
+        poster: "https://example.com/season2-poster.jpg",
         episodes: [
           {
-            title: 'Chapter One: Madmax',
-            url: 'https://example.com/episode3'
+            title: "Chapter One: Madmax",
+            url: "https://example.com/episode3",
           },
           {
-            title: 'Chapter Two: Trick or Treat, Freak',
-            url: 'https://example.com/episode4'
-          }
-        ]
-      }
-    ]
+            title: "Chapter Two: Trick or Treat, Freak",
+            url: "https://example.com/episode4",
+          },
+        ],
+      },
+    ],
   });
-  
+
   // // Save the series to the database
   // newSeries.save().then(() => {
   //   console.log('Series saved successfully!');
@@ -73,21 +74,22 @@ app.get("/", async(req, res) => {
   //   console.error('Failed to save series:', error);
   // });
   const newMovie = new Movie({
-    title: 'Inception',
-    url: 'https://example.com/inception',
-    poster: 'https://example.com/inception-poster.jpg',
-    story: 'A thief with the ability to enter people\'s dreams is hired to plant an idea into a CEO\'s mind.',
-    category: ['Action', 'Thriller', 'Science Fiction'],
+    title: "Inception",
+    url: "https://example.com/inception",
+    poster: "https://example.com/inception-poster.jpg",
+    story:
+      "A thief with the ability to enter people's dreams is hired to plant an idea into a CEO's mind.",
+    category: ["Action", "Thriller", "Science Fiction"],
     year: 2010,
   });
-  
+
   // Save the movie to the database
   // newMovie.save().then(() => {
   //   console.log('Movie saved successfully!');
   // }).catch((error) => {
   //   console.error('Failed to save movie:', error);
   // });
- const s=await Series.find({})
+  const s = await Series.find({});
   res.status(200).json(s);
 });
 
